930dd5e26de5ac946583ce3512ab35a628edef91
Cyril Koenig <cyril27koenig@gmail.com>
2024-10-25 15:09:29 +0200

    vip: Use wires for gate logic
    >  The following expression is illegally connected to a gate.
    >  Variables cannot be connected to the output of gates with driving strength.
    >  Expression: spih_sd_i[0] of type: logic
    >  Please refer to section "Gate and switch level modeling" in IEEE standards
    >  1364 (Verilog) or 1800 (SystemVerilog).

--- target/sim/src/vip_cheshire_soc.sv
+++ target/sim/src/vip_cheshire_soc.sv
@@ -954,20 +954,20 @@ endmodule
 
 module vip_cheshire_soc_tristate import cheshire_pkg::*; (
   // I2C pad IO
-  output logic  i2c_sda_i,
-  input  logic  i2c_sda_o,
-  input  logic  i2c_sda_en,
-  output logic  i2c_scl_i,
-  input  logic  i2c_scl_o,
-  input  logic  i2c_scl_en,
+  output wire i2c_sda_i,
+  input  wire i2c_sda_o,
+  input  wire i2c_sda_en,
+  output wire i2c_scl_i,
+  input  wire i2c_scl_o,
+  input  wire i2c_scl_en,
   // SPI host pad IO
-  input  logic                  spih_sck_o,
-  input  logic                  spih_sck_en,
-  input  logic [SpihNumCs-1:0]  spih_csb_o,
-  input  logic [SpihNumCs-1:0]  spih_csb_en,
-  output logic [ 3:0]           spih_sd_i,
-  input  logic [ 3:0]           spih_sd_o,
-  input  logic [ 3:0]           spih_sd_en,
+  input  wire                  spih_sck_o,
+  input  wire                  spih_sck_en,
+  input  wire [SpihNumCs-1:0]  spih_csb_o,
+  input  wire [SpihNumCs-1:0]  spih_csb_en,
+  output wire [ 3:0]           spih_sd_i,
+  input  wire [ 3:0]           spih_sd_o,
+  input  wire [ 3:0]           spih_sd_en,
   // I2C wires
   inout  wire i2c_sda,
   inout  wire i2c_scl,
