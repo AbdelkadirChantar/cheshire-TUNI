89ccb67af1da6c33a0143407e9cb5b51c1dac285
Abdesattar Kalache <abdesattar.kalache@tuni.fi>
2024-11-13 16:49:28 +0200

    updated DDR4 ip instantiation

--- target/xilinx/scripts/impl_ip.tcl
+++ target/xilinx/scripts/impl_ip.tcl
@@ -38,12 +38,13 @@ switch $proj {
                     CONFIG.C0.DDR4_AxiSelection {true} \
                     ] [get_ips $proj]
             }
-            vcu118 {
-                set_property -dict [list CONFIG.C0.DDR4_Clamshell {true} CONFIG.C0_DDR4_BOARD_INTERFACE {ddr4_sdram} CONFIG.System_Clock {No_Buffer} CONFIG.Reference_Clock {No_Buffer} CONFIG.C0.DDR4_InputClockPeriod {10000} CONFIG.C0.DDR4_CLKOUT0_DIVIDE {3} CONFIG.C0.DDR4_MEMORYPART {MT40A512M16HA-075E} CONFIG.C0.DDR4_CASWRITELATENCY {12} CONFIG.C0.DDR4_CASLATENCY {17} CONFIG.C0.DDR4_DataWidth {72} CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} CONFIG.C0.DDR4_Ecc {true} CONFIG.C0.DDR4_AxiDataWidth {512} CONFIG.C0.DDR4_AxiAddressWidth {32} CONFIG.C0.DDR4_AxiIDWidth {8} CONFIG.ADDN_UI_CLKOUT1_FREQ_HZ {103} CONFIG.C0.BANK_GROUP_WIDTH {1} CONFIG.C0.CS_WIDTH {2} CONFIG.C0.DDR4_AxiSelection {true} ] [get_ips $proj]
-            }
+
+            vcu118 {set_property -dict [list CONFIG.C0.DDR4_Clamshell {true} CONFIG.C0.DDR4_InputClockPeriod {8000} CONFIG.C0.DDR4_CLKOUT0_DIVIDE {3} CONFIG.C0.DDR4_DataWidth {72} CONFIG.C0.DDR4_DataMask {NO_DM_NO_DBI} CONFIG.C0.DDR4_Ecc {true} CONFIG.C0.DDR4_AxiSelection {true} CONFIG.C0.DDR4_AxiDataWidth {512} CONFIG.C0.DDR4_AxiAddressWidth {32} CONFIG.C0.DDR4_AxiIDWidth {8} CONFIG.C0.DDR4_isCustom {false} CONFIG.C0.CS_WIDTH {2}] [get_ips $proj]}
+
             default { nocfgexit $proj $board }
         }
     }
+    
 
     clkwiz {
         create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name $proj
