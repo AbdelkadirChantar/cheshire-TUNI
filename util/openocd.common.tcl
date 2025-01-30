b33067ea32323088533bfe96425e9539fc3d68f0
dkabka <abdesattar.kalache@tuni.fi>
2025-01-30 17:33:38 +0200

    vcu118 openocd config

--- util/openocd.common.tcl
+++ util/openocd.common.tcl
@@ -5,29 +5,32 @@
 # Common OpenOCD script for Cheshire.
 
 transport select jtag
-telnet_port disabled
-tcl_port disabled
+gdb port 3333
+telnet port disabled
+tcl port disabled
 reset_config none
 
 set _CHIPNAME riscv
 jtag newtap $_CHIPNAME cpu -irlen ${irlen} -expected-id 0x1c5e5db3
 
 set _TARGETNAME $_CHIPNAME.cpu
-target create $_TARGETNAME riscv -chain-position $_TARGETNAME -coreid 0
+target create $_TARGETNAME riscv -chain-position $_TARGETNAME 
 
-gdb_report_data_abort enable
-gdb_report_register_access_error enable
+gdb report_data_abort enable
+gdb report_register_access_error enable
 
-riscv set_reset_timeout_sec 120
 riscv set_command_timeout_sec 120
 
-riscv set_prefer_sba off
+# riscv set_prefer_sba off
 
 # Try enabling address translation (only works for newer versions)
-if { [catch { riscv set_enable_virtual on } ] } {
+if { [catch { riscv virt2phys_mode on } ] } {
     echo "Warning: This version of OpenOCD does not support address translation.\
         To debug on virtual addresses, please update to the latest version."
 }
+# dump jtag chain
+scan_chain
+targets
 
 init
 halt
---
+++ util/openocd.vcu118.tcl
@@ -0,0 +1,19 @@
+# Copyright 2024 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# OpenOCD script for Cheshire on Genesys2.
+adapter driver ftdi
+adapter serial FT5X1XMQ; # either comment out or modify to match adapter
+
+adapter speed 1000
+ftdi vid_pid 0x0403 0x6010
+#ftdi layout_init 0x00e8 0x60eb
+ftdi layout_init 0x0088 0x008b
+ftdi channel 1
+set irlen 5
+
+source [file dirname [info script]]/openocd.common.tcl
+
+
+
