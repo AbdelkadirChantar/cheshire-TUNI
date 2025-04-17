5576b282e48dc76c09456aad810bc1b660df2daa
dkabka <abdesattar.kalache@tuni.fi>
2025-04-17 11:39:41 +0300

    dump openocd changes

--- util/openocd.common.tcl
+++ util/openocd.common.tcl
@@ -2,6 +2,7 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
+# Abdelkadir Chantar <abdelkadir.chantar@tuni.fi>
 # Common OpenOCD script for Cheshire.
 
 transport select jtag
@@ -16,25 +17,32 @@ jtag newtap $_CHIPNAME cpu -irlen ${irlen} -expected-id 0x1c5e5db3
 set _TARGETNAME $_CHIPNAME.cpu
 target create $_TARGETNAME riscv -chain-position $_TARGETNAME 
 
-# gdb report_data_abort enable
-# gdb report_register_access_error enable
 
-gdb report_data_abort enable
-gdb report_register_access_error enable
-
+#gdb_report_data_abort enable
+#gdb_report_register_access_error enable
 riscv set_command_timeout_sec 120
+#riscv set_vlenb 0
+#riscv set_mem_access progbuf
 
-# riscv set_prefer_sba off
+#riscv set_prefer_sba off
+
+# Exit when debugger detaches
+$_TARGETNAME configure -event gdb-detach {
+    echo "GDB detached; ending debugging session."
+    shutdown
+}
 
 # Try enabling address translation (only works for newer versions)
 if { [catch { riscv virt2phys_mode on } ] } {
     echo "Warning: This version of OpenOCD does not support address translation.\
         To debug on virtual addresses, please update to the latest version."
 }
+
 # dump jtag chain
-scan_chain
-targets
+#scan_chain
+#targets
 
 init
-halt
-echo "Ready for Remote Connections"
+reset halt
+echo "Ready for Remote Connections."
+# if progbuf is not intended to be read from, why the read_progbuf is being called in the first place?
