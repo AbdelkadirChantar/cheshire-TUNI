0aba487743b629fc0e42ac6a90945e24e8ce571e
krabch <abdelkadir.chantar@tuni.fi>
2025-05-05 13:26:27 +0300

    document required openocd version 0.12

--- util/openocd.common.tcl
+++ util/openocd.common.tcl
@@ -2,12 +2,15 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
+# Abdelkadir Chantar <abdelkadir.chantar@tuni.fi>
 # Common OpenOCD script for Cheshire.
+#
+# !!!: Must use exactly OpenOCD 0.12
 
 transport select jtag
-gdb port 3333
-telnet port disabled
-tcl port disabled
+#gdb port 3333
+#telnet port disabled
+#tcl port disabled
 reset_config none
 
 set _CHIPNAME riscv
@@ -16,25 +19,31 @@ jtag newtap $_CHIPNAME cpu -irlen ${irlen} -expected-id 0x1c5e5db3
 set _TARGETNAME $_CHIPNAME.cpu
 target create $_TARGETNAME riscv -chain-position $_TARGETNAME 
 
-# gdb report_data_abort enable
-# gdb report_register_access_error enable
 
-gdb report_data_abort enable
-gdb report_register_access_error enable
+#gdb report_data_abort enable
+#gdb report_register_access_error enable
 
-riscv set_command_timeout_sec 120
+riscv set_command_timeout_sec 2
+#riscv set_vlenb 0
+#riscv set_mem_access progbuf
 
-# riscv set_prefer_sba off
+riscv set_mem_access sysbus
+
+# Exit when debugger detaches
+$_TARGETNAME configure -event gdb-detach {
+    echo "GDB detached; ending debugging session."
+    shutdown
+}
 
 # Try enabling address translation (only works for newer versions)
-if { [catch { riscv virt2phys_mode on } ] } {
+if { [catch { riscv set_enable_virtual on } ] } {
     echo "Warning: This version of OpenOCD does not support address translation.\
         To debug on virtual addresses, please update to the latest version."
 }
+
 # dump jtag chain
 scan_chain
 targets
 
 init
 halt
-echo "Ready for Remote Connections"
--- util/openocd.zcu.104.tcl
+++ util/openocd.zcu.104.tcl
@@ -2,13 +2,14 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 #
-# OpenOCD script for Cheshire on Genesys2.
+# Abdelkadir Chantar <abdelkadir.chantar@tuni.fi>
+# OpenOCD script for Cheshire on zcu104.
+
 adapter driver ftdi
 adapter serial FT5X1XMQ; # either comment out or modify to match adapter
 
 adapter speed 1000
 ftdi vid_pid 0x0403 0x6010
-#ftdi layout_init 0x00e8 0x60eb
 ftdi layout_init 0x0088 0x008b
 ftdi channel 1
 set irlen 5
