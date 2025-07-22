8a4e755bddbab0056816c2d6caea652ea1ee74ca
krabch <abdelkadir.chantar@tuni.fi>
2025-07-22 15:15:33 +0300

    target/util: dual core setup

--- util/openocd.common.tcl
+++ util/openocd.common.tcl
@@ -17,8 +17,12 @@ reset_config none
 set _CHIPNAME riscv
 jtag newtap $_CHIPNAME cpu -irlen ${irlen} -expected-id 0x1c5e5db3
 
-set _TARGETNAME $_CHIPNAME.cpu
-target create $_TARGETNAME riscv -chain-position $_TARGETNAME 
+set _TARGETNAME0 $_CHIPNAME.cpu0
+target create $_TARGETNAME0 riscv -chain-position $_CHIPNAME.cpu -coreid 0
+
+# Note: You can add more cores by incrementing the coreid and target name
+set _TARGETNAME1 $_CHIPNAME.cpu1
+target create $_TARGETNAME1 riscv -chain-position $_CHIPNAME.cpu -coreid 1
 
 
 #gdb report_data_abort enable
@@ -30,11 +34,6 @@ riscv set_command_timeout_sec 2
 
 riscv set_mem_access sysbus
 
-# Exit when debugger detaches
-$_TARGETNAME configure -event gdb-detach {
-    echo "GDB detached; ending debugging session."
-    shutdown
-}
 
 # Try enabling address translation (only works for newer versions)
 if { [catch { riscv set_enable_virtual on } ] } {
